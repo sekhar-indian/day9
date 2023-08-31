d3b96c8987990d3a9a0368f738d814b8b323adc8
sekhar-indian <munisekharudavalapati1@gmail.com>
2023-08-31 10:44:39 +0530

    day2

--- day9.js
+++ day9.js
@@ -1,1 +1,20 @@
- 
+
+var form=document.getElementById("addform");
+
+form.addEventListener("submit",formf)
+
+function formf(e)
+{
+    e.preventDefault();
+
+    var email=document.getElementById("email");
+    var name=document.getElementById("name");
+    var pnumber=document.getElementById("pnumber");
+    var password=document.getElementById("password");
+    alert("Apointment Booking Success")
+    console.log(email.value)
+    document.write("email id= "+email.value+"<br>")
+    document.write("Name = "+name.value+ "<br>")
+    document.write("Pnumber = ",pnumber.value)
+    
+}
