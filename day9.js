f8f8a99060b2dcbf210b01b7693408c33992e711
sekhar-indian <munisekharudavalapati1@gmail.com>
2023-09-02 13:40:50 +0530

    day3

--- day9.js
+++ day9.js
@@ -1,20 +1,46 @@
+// var form=document.getElementById("addform");
 
-var form=document.getElementById("addform");
+// form.addEventListener("submit",formf)
 
-form.addEventListener("submit",formf)
+// function formf(e)
+// {
+//     e.preventDefault();
 
-function formf(e)
-{
-    e.preventDefault();
-
-    var email=document.getElementById("email");
-    var name=document.getElementById("name");
-    var pnumber=document.getElementById("pnumber");
-    var password=document.getElementById("password");
-    alert("Apointment Booking Success")
-    console.log(email.value)
-    document.write("email id= "+email.value+"<br>")
-    document.write("Name = "+name.value+ "<br>")
-    document.write("Pnumber = ",pnumber.value)
+//     var email=document.getElementById("email");
+//     var name=document.getElementById("name");
+//     var pnumber=document.getElementById("pnumber");
+//     var password=document.getElementById("password");
+//     alert("Apointment Booking Success")
+//     console.log(email.value)
+//     document.write("email id= "+email.value+"<br>")
+//     document.write("Name = "+name.value+ "<br>")
+//     document.write("Pnumber = ",pnumber.value)
     
+// }
+
+
+var va=document.getElementById("addform");
+
+va.addEventListener("submit",formdata);
+
+function formdata(e){
+     e.preventDefault();
+    
+       var email=document.getElementById("email").value;
+       var name=document.getElementById("name").value;
+       var pnumber=document.getElementById("pnumber").value;
+       var password=document.getElementById("password").value;
+   
+    
+    
+    //desplay 
+    var d=document.createElement("lable");
+    d.className="class1";
+    d.id="id1";
+    d.createElement="br";
+    d.appendChild(document.createTextNode(email));
+    va.appendChild(d);
+    console.log(d);
+
+        
 }
